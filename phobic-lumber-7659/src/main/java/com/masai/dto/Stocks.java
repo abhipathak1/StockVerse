103f9b5d71fb8060e10fee4a6b6eea1024b2278d
Abhay Pathak <123934582+abhipathak1@users.noreply.github.com>
2023-07-21 20:39:37 +0530

    Add files via upload

--- phobic-lumber-7659/src/main/java/com/masai/dto/Stocks.java
+++ phobic-lumber-7659/src/main/java/com/masai/dto/Stocks.java
@@ -1,5 +1,76 @@
 package com.masai.dto;
 
-public class Stocks {
+import java.util.List;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "stocks")
+public class Stocks {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "stock_id")
+	private Long stockId;
+
+	@Column(name = "stock_name", nullable = false)
+	private String stockName;
+
+	@Column(name = "stock_price", nullable = false)
+	private double stockPrice;
+
+	public Stocks() {
+		super();
+	}
+
+	public Stocks(Long stockId, String stockName, double stockPrice, List<UserPortfolio> userPortfolioList) {
+		super();
+		this.stockId = stockId;
+		this.stockName = stockName;
+		this.stockPrice = stockPrice;
+		this.userPortfolioList = userPortfolioList;
+	}
+
+	public Long getStockId() {
+		return stockId;
+	}
+
+	public void setStockId(Long stockId) {
+		this.stockId = stockId;
+	}
+
+	public String getStockName() {
+		return stockName;
+	}
+
+	public void setStockName(String stockName) {
+		this.stockName = stockName;
+	}
+
+	public double getStockPrice() {
+		return stockPrice;
+	}
+
+	public void setStockPrice(double stockPrice) {
+		this.stockPrice = stockPrice;
+	}
+
+	public List<UserPortfolio> getUserPortfolioList() {
+		return userPortfolioList;
+	}
+
+	public void setUserPortfolioList(List<UserPortfolio> userPortfolioList) {
+		this.userPortfolioList = userPortfolioList;
+	}
+
+	// One-to-Many relationship with UserPortfolio
+	@OneToMany(mappedBy = "stock", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+	private List<UserPortfolio> userPortfolioList;
 }
--- phobic-lumber-7659/src/main/java/com/masai/dto/UserPortfolio.java
+++ phobic-lumber-7659/src/main/java/com/masai/dto/UserPortfolio.java
@@ -1,5 +1,76 @@
 package com.masai.dto;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "user_portfolio")
 public class UserPortfolio {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "portfolio_id")
+	private Long portfolioId;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id", nullable = false)
+	private User user;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "stock_id", nullable = false)
+	private Stocks stock;
+
+	@Column(name = "quantity", nullable = false)
+	private int quantity;
+
+	public UserPortfolio() {
+		super();
+	}
+
+	public UserPortfolio(Long portfolioId, User user, Stocks stock, int quantity) {
+		super();
+		this.portfolioId = portfolioId;
+		this.user = user;
+		this.stock = stock;
+		this.quantity = quantity;
+	}
+
+	public Long getPortfolioId() {
+		return portfolioId;
+	}
+
+	public void setPortfolioId(Long portfolioId) {
+		this.portfolioId = portfolioId;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public Stocks getStock() {
+		return stock;
+	}
+
+	public void setStock(Stocks stock) {
+		this.stock = stock;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
 
 }
